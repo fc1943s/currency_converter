daf9d267b8026344bf4c09a7440211e9fd88c7b1
프리마 <fc1943s@pm.me>
2024-06-14 19:16:18 -0400

    added bun

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,11 +3,11 @@ param($fast)
 if (!$fast) {
     mix setup
 
+    ~/.bun/bin/bun install --cwd assets --frozen-lockfile
+
     mix test
 
     mix assets.deploy
-
-    bun install --cwd assets --frozen-lockfile
 }
 
 mix credo
--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,2 +1,10 @@
 mix do local.rebar --force, local.hex --force
-mix escript.install github livebook-dev/livebook
+mix escript.install github livebook-dev/livebook --force
+
+curl -fsSL https://bun.sh/install | bash
+$env:PATH = "~/.bun/bin:$env:PATH"
+
+if ($IsLinux) {
+    apt update
+    apt install -y inotify-tools
+}
